8aef89e178bba5a0c05c808c2d1551d31fe1cc85
GivPenjinun <penjinun.watt@gmail.com>
2023-12-23 13:38:19 +0700

    feat: add suggestion section

--- client/src/components/BlogContent.jsx
+++ client/src/components/BlogContent.jsx
@@ -6,7 +6,10 @@ const BlogContent = () => {
   return (
     <main className="my-20 px-20 flex gap-12 w-full min-h-[350px]">
       <div className="w-4/5 flex flex-col gap-8">
-        <img src={dog} className="h-[350px] " />
+        <div className="w-full flex justify-center">
+          <img src={dog} className="w-[650px] h-[350px] " />
+        </div>
+
         <div className="h-[80px] flex items-center gap-4">
           <img src={dog} className="h-[70px] w-[70px] rounded-full " />
           <div className="">
--- client/src/components/Suggest.jsx
+++ client/src/components/Suggest.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import dog from "../assets/dog-bobo.jpg";
 
 const Suggest = () => {
-  return <div>Suggest</div>;
+  return (
+    <div className="flex flex-col gap-3">
+      <h3 className="text-headLine4">Other posts you may like</h3>
+      <div className="flex flex-col gap-1">
+        <img src={dog} alt="" className="" />
+        <h2 className="text-headLine3">lo</h2>
+        <button className="text-body3 rounded-sm px-1 border-2 border-primaryBlue1 hover:bg-primaryBlue2">
+          Read More
+        </button>
+      </div>
+      {/*
+    {posts.map(post)=>(<div key={post.id} className="flex flex-col gap-1">
+        <img src={post.image} alt="" className="" />
+        <h2 className="text-headLine3">{post.title}</h2>
+        <button className="text-body3 rounded-sm px-1 border-2 border-primaryBlue1 hover:bg-primaryBlue2">
+          Read More
+        </button>
+      </div>)}
+    */}
+    </div>
+  );
 };
 
 export default Suggest;
