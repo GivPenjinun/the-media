bfa46054057b0cef8143759ea58a8ad7500fc54f
GivPenjinun <penjinun.watt@gmail.com>
2024-01-05 15:15:34 +0700

    refactot: change key for database

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ import mysql from "mysql2";
 import "dotenv/config";
 
 export const db = mysql.createConnection({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
+  database: process.env.DBNAME,
 });
 
 db.connect((error) => {
